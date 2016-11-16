3c597abcd3977a7a8083f528482050ab09a28c39
Jovana <jovana.nikolic@gmail.com>
2016-11-16 21:59:40 +0100

    swagger prereq

--- Test2/Global.asax.cs
+++ Test2/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NSwag.AspNet.Owin;
 
 namespace Test2
 {
@@ -13,6 +14,13 @@ namespace Test2
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapOwinPath("swagger", app =>
+            {
+                app.UseSwaggerUi(typeof(WebApiApplication).Assembly, new SwaggerUiOwinSettings
+                {
+                    MiddlewareBasePath = "/swagger"
+                });
+            });
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
